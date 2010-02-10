02ade89f1226a9c9a3099b5162dd2bfe742a77df
isaacs <i@izs.me>
2010-02-10 00:48:36 -0800

    More examples.

--- examples/example.js
+++ examples/example.js
@@ -2,24 +2,26 @@
 var posix = require("posix"),
   sys = require("sys"),
   path = require("path"),
-  xml = posix.cat(path.join(path.dirname(__filename), "test.xml")),
+  xml = posix.cat(path.join(__dirname, "test.xml")),
   sax = require("../lib/sax"),
   strict = sax.parser(true),
   loose = sax.parser(),
   inspector = function (ev) { return function (data) {
+    sys.error("");
     sys.error(ev+": "+sys.inspect(data));
+    // for (var i in data) sys.error(i+ " "+sys.inspect(data[i]));
+    sys.error(this.line+":"+this.column);
   }};
 
 xml.addCallback(function (xml) {
   // strict.write(xml);
   
-  ["processingInstruction",
-      "text", "comment","opentag",
-      "closetag","attribute"].forEach(function (ev) {
+  sax.EVENTS.forEach(function (ev) {
     loose["on"+ev] = inspector(ev);
   });
   loose.onend = function () {
     sys.error("end");
+    // sys.error(sys.inspect(loose));
   };
   
   loose.write(xml).close();
