9ae6210ad595e42e35f468884c50b06bb5dbd06a
isaacs <i@izs.me>
2010-02-11 11:47:57 -0800

    Random stuff. Testing a bigger xml file.

--- examples/example.js
+++ examples/example.js
@@ -3,14 +3,14 @@ var posix = require("posix"),
   sys = require("sys"),
   path = require("path"),
   xml = posix.cat(path.join(__dirname, "test.xml")),
-  sax = require("../lib/sax"),
+  sax = require("../lib/sax-trampoline"),
   strict = sax.parser(true),
   loose = sax.parser(false, {trim:true}),
   inspector = function (ev) { return function (data) {
-    sys.error("");
-    sys.error(ev+": "+sys.inspect(data));
+    // sys.error("");
+    // sys.error(ev+": "+sys.inspect(data));
     // for (var i in data) sys.error(i+ " "+sys.inspect(data[i]));
-    sys.error(this.line+":"+this.column);
+    // sys.error(this.line+":"+this.column);
   }};
 
 xml.addCallback(function (xml) {
@@ -20,16 +20,22 @@ xml.addCallback(function (xml) {
     loose["on"+ev] = inspector(ev);
   });
   loose.onend = function () {
-    sys.error("end");
-    sys.error(sys.inspect(loose));
+    // sys.error("end");
+    // sys.error(sys.inspect(loose));
   };
   
   // do this one char at a time to verify that it works.
-  (function () {
-    if (xml) {
-      loose.write(xml.charAt(0));
-      xml = xml.slice(1);
-      process.nextTick(arguments.callee);
-    } else loose.close();
-  })();
+  // (function () {
+  //   if (xml) {
+  //     loose.write(xml.substr(0,1000));
+  //     xml = xml.substr(1000);
+  //     process.nextTick(arguments.callee);
+  //   } else loose.close();
+  // })();
+  
+  for (var i = 0; i < 1000; i ++) {
+    loose.write(xml);
+    loose.close();
+  }
+
 });
