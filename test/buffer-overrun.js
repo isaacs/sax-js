82d0ddff0beadaa3cbcf0f970ecb50f2f56498e9
isaacs <i@izs.me>
2010-02-21 15:41:22 -0800

    Simplify the test script a little bit by removing some unnecessary cruft.

--- test/buffer-overrun.js
+++ test/buffer-overrun.js
@@ -1,11 +1,7 @@
-var path = require("path"),
-  sax = require(path.join(__dirname, "../lib/sax")),
-  common = require(__dirname);
-
 // set this really low so that I don't have to put 64 MB of xml in here.
-sax.MAX_BUFFER_LENGTH = 5;
+require(require("path").join(__dirname, "../lib/sax")).MAX_BUFFER_LENGTH = 5;
 
-var p = common.test({
+require(__dirname).test({
   expect : [
     ["error", "Max buffer length exceeded: tagName\nLine: 0\nColumn: 15\nChar: "],
     ["error", "Max buffer length exceeded: tagName\nLine: 0\nColumn: 30\nChar: "],
@@ -17,24 +13,7 @@ var p = common.test({
     ["text", "yo"],
     ["closetag", "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"]
   ]
-});
-
-
-
-// 
-// sax.EVENTS.forEach(function (ev) {
-//   p["on"+ev] = function (arg) {
-//     sys.error("EVENT: "+ev+" "+sys.inspect(arg));
-//   };
-// });
-
-// p.onerror = function (er) {
-//   sys.debug(er.message);
-//   this.resume();
-// };
-
-
-p .write("<abcdefghijklmn")
+}).write("<abcdefghijklmn")
   .write("opqrstuvwxyzABC")
   .write("DEFGHIJKLMNOPQR")
   .write("STUVWXYZ>")
