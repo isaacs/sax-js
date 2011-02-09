321b285c4e4d4862d802ce8681adecb513dcb0de
Laurie Harper <laurie@holoweb.net>
2011-02-08 22:42:21 -0500

    Add `opencdata` and `closecdata` events
    So it's possible to detect the difference between a CDATA section
    split over multiple writes, and multiple successive CDATA sections.

--- test/cdata-chunked.js
+++ test/cdata-chunked.js
@@ -2,8 +2,10 @@
 require(__dirname).test({
   expect : [
     ["opentag", {"name": "R","attributes": {}}],
+    ["opencdata", undefined],
     ["cdata", " this is "],
     ["cdata", "character data  "],
+    ["closecdata", undefined],
     ["closetag", "R"]
   ]
 }).write("<r><![CDATA[ this is ").write("character data  ").write("]]></r>").close();
