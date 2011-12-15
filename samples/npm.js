58e2250a5b06a952bf8efc3d9c85bbe78944dfc3
Nuno Job <nunojobpinto@gmail.com>
2011-12-15 17:35:41 +0000

    [openobject] bang

--- samples/npm.js
+++ samples/npm.js
@@ -1,23 +1,26 @@
 var fs            = require('fs')
-  , request       = require('request')
   , sax           = require('../lib/sax-json.js')
   , parse_stream  = sax.createStream()
-  , get_stream    =
-    request('http://isaacs.couchone.com/registry/_all_docs?include_docs=true')
   ;
 
-  get_stream.pipe(parse_stream);
-
-stream.on('error', function(er) {
-  console.log(err);
+parse_stream.on('error', function(er) {
+  console.log(er);
 });
 
-saxStream.on("opentag", function (node) {
-  console.log(node);
+parse_stream.on("openobject", function (key) {
+  console.log(key);
 });
+//
+//parse_stream.on("end", function () {
+//  console.log("im done");
+//});
+//
+//parse_stream.on("start", function () {
+//  console.log("im done");
+//});
+//
+//parse_stream.on("ready", function () {
+//  console.log("im done");
+//});
 
-saxStream.on("end", function () {
-  console.log("im done");
-});
-
-// fs.writeFileSync('npm.json', JSON.stringify(dict.export(),null,2));
+fs.createReadStream("foo.json").pipe(parse_stream);
