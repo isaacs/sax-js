303e69aa2681be8805a96efea8d23f882741c097
Nuno Job <nunojobpinto@gmail.com>
2011-12-17 21:41:41 +0000

    [test] json.org sample added to tests

--- test/clarinet.js
+++ test/clarinet.js
@@ -80,6 +80,71 @@ var seps   = [undefined, /\t|\n|\r| /, '']
         , ['ready'       , undefined]
         ]
       }
+    , json_org  :
+      { text    : 
+          ('{\r\n' +
+          '          "glossary": {\n' +
+          '              "title": "example glossary",\n\r' +
+          '      \t\t"GlossDiv": {\r\n' +
+          '                  "title": "S",\r\n' +
+          '      \t\t\t"GlossList": {\r\n' +
+          '                      "GlossEntry": {\r\n' +
+          '                          "ID": "SGML",\r\n' +
+          '      \t\t\t\t\t"SortAs": "SGML",\r\n' +
+          '      \t\t\t\t\t"GlossTerm": "Standard Generalized ' + 
+          'Markup Language",\r\n' +
+          '      \t\t\t\t\t"Acronym": "SGML",\r\n' +
+          '      \t\t\t\t\t"Abbrev": "ISO 8879:1986",\r\n' +
+          '      \t\t\t\t\t"GlossDef": {\r\n' +
+          '                              "para": "A meta-markup language,' +
+          ' used to create markup languages such as DocBook.",\r\n' +
+          '      \t\t\t\t\t\t"GlossSeeAlso": ["GML", "XML"]\r\n' +
+          '                          },\r\n' +
+          '      \t\t\t\t\t"GlossSee": "markup"\r\n' +
+          '                      }\r\n' +
+          '                  }\r\n' +
+          '              }\r\n' +
+          '          }\r\n' +
+          '      }\r\n')
+      , events  :
+        [ ["openobject"  , "glossary"]
+        , ["openobject"  , "title"]
+        , ['value'       , 'example glossary']
+        , ["key"         , "GlossDiv"]
+        , ["openobject"  , "title"]
+        , ['value'       , 'S']
+        , ["key"         , "GlossList"]
+        , ["openobject"  , "GlossEntry"]
+        , ["openobject"  , "ID"]
+        , ['value'       , 'SGML']
+        , ["key"         , "SortAs"]
+        , ['value'       , 'SGML']
+        , ["key"         , "GlossTerm"]
+        , ['value'       , 'Standard Generalized Markup Language']
+        , ["key"         , "Acronym"]
+        , ['value'       , 'SGML']
+        , ["key"         , "Abbrev"]
+        , ['value'       , 'ISO 8879:1986']
+        , ["key"         , "GlossDef"]
+        , ["openobject"  , "para"]
+        , ['value'       , 'A meta-markup language, used to create markup languages such as DocBook.']
+        , ["key"         , "GlossSeeAlso"]
+        , ['openarray'   , undefined]
+        , ['value'       , 'GML']
+        , ['value'       , 'XML']
+        , ['closearray'  , undefined]
+        , ["closeobject" , undefined]
+        , ["key"         , "GlossSee"]
+        , ["value"       , "markup"]
+        , ["closeobject" , undefined]
+        , ["closeobject" , undefined]
+        , ["closeobject" , undefined]
+        , ["closeobject" , undefined]
+        , ["closeobject" , undefined]
+        , ['end'         , undefined]
+        , ['ready'       , undefined]
+        ]
+      }
     };
 
 function generic(key,sep) {
