d7aeb040e73289797db23abc29f6ed67547250ce
Nuno Job <nunojobpinto@gmail.com>
2011-12-19 20:21:47 +0000

    [tests] added twitter

--- test/npm.js
+++ test/npm.js
@@ -1,19 +1,29 @@
-var fs            = require('fs')
-  , clarinet      = require('../clarinet.js')
-  , parse_stream  = clarinet.createStream()
-  , assert        = require('assert')
+var fs             = require('fs')
+  , clarinet       = require('../clarinet.js')
+  , npm_stream     = clarinet.createStream()
+  , twitter_stream = clarinet.createStream()
+  , assert         = require('assert')
   ;
 
 describe('clarinet', function(){
   describe('#npm', function() {
     it('should be able to parse npm', function (done){
-      parse_stream.on("error", function (err) { done(err); });
-      parse_stream.on("end", function () {
+      npm_stream.on("error", function (err) { done(err); });
+      npm_stream.on("end", function () {
         assert(true, "parsing worked");
         done();
       });
-      fs.createReadStream(__dirname + '/../samples/npm2.json')
-        .pipe(parse_stream);
+      fs.createReadStream(__dirname + '/../samples/npm.json')
+        .pipe(npm_stream);
+    });
+    it('should be able to parse twitter', function (done){
+      twitter_stream.on("error", function (err) { done(err); });
+      twitter_stream.on("end", function () {
+        assert(true, "parsing worked");
+        done();
+      });
+      fs.createReadStream(__dirname + '/../samples/twitter.json')
+        .pipe(twitter_stream);
     });
   });
 });
