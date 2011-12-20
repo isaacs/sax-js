0b611b7f7c96673f3d006eb2fd4f87bd289bf70e
Nuno Job <nunojobpinto@gmail.com>
2011-12-20 17:16:26 +0000

    [bench] added async tests

--- bench/async.js
+++ bench/async.js
@@ -1,50 +1,80 @@
-// forked from github.com/creationix/jsonparser
+// forked from github.com/creationix/jsonparse
+// npm install jsonparse async
 
-var fs       = require('fs')
-  , clarinet = require('../clarinet')
-  , start    = { parser: null, stream: null }
-  , samples  = ['npm.json', 'twitter.json']
-  , stream   = clarinet.createStream()
-  , parser   = clarinet.parser()
+// node bench/async.js samples/npm.json
+//                    jsonfile         
+console.log('=N("node bench/async.js ' + process.argv[2] + '")');
+console.log('=N("clp (clarinet parser), cls (clarinet event emitter)")');
+console.log('=N("jpp (creationix/jsonparse)")');
+
+var fs         = require('fs')
+  , clarinet   = require('../clarinet')
+  , Parser     = require('jsonparse')
+  , jsonparser
+  , p
+  , s
+  , start
   ;
 
-parser.onend = function () { 
-  console.log('parser: %s', Date.now()-start.parser);
-};
-
-stream.on('end', function () {
-  console.log('stream: %s', Date.now()-start.stream);
-});
-
-for(var i in samples) {a
-  var current = samples[i];
-  console.log('$', current);
-  
-  run_test(current, process.ARGV[2]);
+function stream_bench(cb) {
+  s          = clarinet.createStream()
+  s.on('end', function () {
+    console.log('cls, %s', Date.now()-start);
+    cb();
+  });
+  start = Date.now();
+  var fs_read = fs.createReadStream(process.argv[2]);
+  fs_read.setEncoding('utf-8');
+  fs_read.pipe(s);
 }
 
-function run_test(current,who) {
-  start[who] = Date.now();
-  var file_stream    = 
-     fs.createReadStream(__dirname + "/../samples/"+current);
-   file_stream.setEncoding('utf-8');
-   file_stream.on('data', function (chunk){
-     
-   });
-  while (true) {
-    var start = Date.now();
-    for (var i = 0; i < 1000; i++) {
-      JSON.parse(json);
-    }
-    var first = Date.now() - start;
-    console.log("JSON.parse took %s", first);
-  
+function parser_bench(cb) {
+  p          = clarinet.parser()
+  p.onend = function () { 
+    console.log('clp, %s', Date.now()-start);
+    cb();
+  };
+  var fs_read = fs.createReadStream(process.argv[2]);
+  fs_read.setEncoding('utf-8');
+  fs_read.on('data', function(chunk) { p.write(chunk); });
+  fs_read.on('end', function () { p.end(); });
+  start = Date.now();
+}
+
+function jsonparse_bench(cb) {
+  jsonparser = new Parser()
+  var fs_read = fs.createReadStream(process.argv[2]);
+  var buffer  = [];
+  var bodyLen = 0;
+  fs_read.on('data', function(chunk) { 
+    buffer.push(chunk);
+    bodyLen += chunk.length;
+  });
+  fs_read.on('end', function () { 
+    var body = new Buffer(bodyLen);
+    var i = 0;
+    buffer.forEach(function (chunk) {
+      chunk.copy(body, i, 0, chunk.length);
+      i += chunk.length;
+    });
     start = Date.now();
-    var p = new Parser();
-    for (var i = 0; i < 1000; i++) {
-      p.write(json);
-    }
-    var second = Date.now() - start;
-    console.log("streaming parser took %s", second);
-    console.log("streaming is %s times slower", second / first);
-  }
+    jsonparser.write(body);
+    console.log('jpp, %s', Date.now()-start);
+    setTimeout(repeat,0);
+  });
+}
+
+function repeat() {
+  stream_bench(function () {
+    return parser_bench(function () { return jsonparse_bench(function(){})})
+  });
+}
+
+repeat();
+//async.whilst(
+//    function () { return true; },
+//    function (callback) {
+//      var fs = [ stream_bench, parser_bench, jsonparse_bench];
+//      async.series(fs, function() { c(); });
+//    }
+//);
--- bench/sync.js
+++ bench/sync.js
@@ -16,35 +16,41 @@ var fs         = require('fs')
   , max        = process.argv[3]
   ;
 
+console.log('=N("node bench/sync.js ' + process.argv[2] + ' ' +
+   process.argv[3] + '")');
+console.log('=N("clp (clarinet parser), cls (clarinet event emitter)")');
+console.log('=N("jpp (creationix/jsonparse), v8s (JSON.parse string)")');
+console.log('=N("v8b (JSON.parse buffer)")');
+
 while (true) {
   try {
     start = Date.now();
     for (var i = 0; i < max; i++) p.write(string);
-    console.log("clp: %s", Date.now()-start);
-  } catch (ex1) { console.log( "clp: -1"); }
+    console.log("clp, %s", Date.now()-start);
+  } catch (ex1) { }
 
   try {
     start = Date.now();
     for (var i = 0; i < max; i++) s.write(string);
-    console.log("cls: %s", Date.now()-start);
-  } catch (ex1) { console.log( "cls: -1"); }
+    console.log("cls, %s", Date.now()-start);
+  } catch (ex1) { }
 
   try {
     start = Date.now();
     for (var i = 0; i < max; i++) jsonparser.write(file);
-    console.log("jpp: %s", Date.now()-start);
-  } catch (ex2) { console.log( "jpp: -1"); }
+    console.log("jpp, %s", Date.now()-start);
+  } catch (ex2) { }
 
   try {
     start = Date.now();
     for (var i = 0; i < max; i++) JSON.parse(string);
-    console.log("v8s: %s", Date.now()-start);
-  } catch (ex3) { console.log( "v8s: -1"); }
+    console.log("v8s, %s", Date.now()-start);
+  } catch (ex3) { }
 
   try {
     start = Date.now();
     for (var i = 0; i < max; i++) JSON.parse(file);
     console.log("v8b: %s", Date.now()-start);
-  } catch (ex4) { console.log( "v8b: -1"); }
+  } catch (ex4) { }
 
 }
