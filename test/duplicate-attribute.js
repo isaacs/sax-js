855ea2cfb5d052f03fc98787d01035b6631bde2c
vvo <v@fasterize.com>
2012-06-18 16:26:08 +0200

    Handle duplicate attributes
    This fixes a bug where the last attribute encountered overwrites all
    others attributes having the same name.
    
    Added test file.

---
+++ test/duplicate-attribute.js
@@ -0,0 +1,14 @@
+// default to uppercase
+require(__dirname).test
+  ( { xml :
+      "<span id=\"hello\" id=\"there\"></span>"
+    , expect :
+      [ [ "attribute", { name: "ID", value: "hello" } ]
+      , [ "opentag", { name: "SPAN",
+                       attributes: { ID: "hello" } } ]
+      , [ "closetag", "SPAN" ]
+      ]
+    , strict : false
+    , opt : {}
+    }
+  )
