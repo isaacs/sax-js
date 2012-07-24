04626843f991bd13f8be32ccc36bc4e77b6dd364
Nuno Job <nunojobpinto@gmail.com>
2012-07-24 15:32:56 +0100

    [test minor] minor fixes in tests for new v8

--- test/clarinet.js
+++ test/clarinet.js
@@ -291,19 +291,6 @@ var seps   = [undefined, /\t|\n|\r/, '']
          , ['ready'        , undefined]
          ]
       }
-    , string_invalid_hex :
-      { text             : 
-        '["foo foo, blah blah \u0123 \u4567 \u89ab \uc/ef \uABCD \uEFFE' +
-        ' bar baz bing"]'
-      , events              :
-        [ ["openarray"    , undefined]
-       , ["value"         , 'foo foo, blah blah \u0123 \u4567 \u89ab \uc/ef'+
-                             ' \uABCD \uEFFE bar baz bing']
-       , ["closearray"    , undefined]
-       , ['end'           , undefined]
-       , ['ready'         , undefined]
-       ]
-      }
     , frekin_string:
       { text    : '["\\\\\\"\\"a\\""]'
       , events  :
