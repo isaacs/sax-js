2495644ea6cc4ef258c7f36bffbbe3774d1859b9
abarre <a@fasterize.com>
2012-08-20 09:37:36 +0200

    Fix detection of </script>

--- test/script.js
+++ test/script.js
@@ -1,10 +1,10 @@
 require(__dirname).test({
-  xml : "<html><head><script>if (1 < 0) { console.log('elo there'); }</script></head></html>",
+  xml : "<html><head><script>if (1 < 0) { console.log('</div>'); }</script></head></html>",
   expect : [
     ["opentag", {"name": "HTML","attributes": {}}],
     ["opentag", {"name": "HEAD","attributes": {}}],
     ["opentag", {"name": "SCRIPT","attributes": {}}],
-    ["script", "if (1 < 0) { console.log('elo there'); }"],
+    ["script", "if (1 < 0) { console.log('</div>'); }"],
     ["closetag", "SCRIPT"],
     ["closetag", "HEAD"],
     ["closetag", "HTML"]
