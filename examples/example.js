9f7313f602017d252f062f36ede9f06025e30767
isaacs <i@izs.me>
2013-01-06 09:22:55 -0800

    Fix example
    close #66

--- examples/example.js
+++ examples/example.js
@@ -1,41 +1,29 @@
 
 var fs = require("fs"),
-  util = require('util'),
+  util = require("util"),
   path = require("path"),
-  xml = fs.cat(path.join(__dirname, "test.xml")),
+  xml = fs.readFileSync(path.join(__dirname, "test.xml"), "utf8"),
   sax = require("../lib/sax"),
   strict = sax.parser(true),
   loose = sax.parser(false, {trim:true}),
   inspector = function (ev) { return function (data) {
-    // util.error("");
-    // util.error(ev+": "+util.inspect(data));
-    // for (var i in data) util.error(i+ " "+util.inspect(data[i]));
-    // util.error(this.line+":"+this.column);
+    console.error("%s %s %j", this.line+":"+this.column, ev, data);
   }};
 
-xml.addCallback(function (xml) {
-  // strict.write(xml);
-  
-  sax.EVENTS.forEach(function (ev) {
-    loose["on"+ev] = inspector(ev);
-  });
-  loose.onend = function () {
-    // util.error("end");
-    // util.error(util.inspect(loose));
-  };
-  
-  // do this one char at a time to verify that it works.
-  // (function () {
-  //   if (xml) {
-  //     loose.write(xml.substr(0,1000));
-  //     xml = xml.substr(1000);
-  //     process.nextTick(arguments.callee);
-  //   } else loose.close();
-  // })();
-  
-  for (var i = 0; i < 1000; i ++) {
-    loose.write(xml);
-    loose.close();
-  }
-
+sax.EVENTS.forEach(function (ev) {
+  loose["on"+ev] = inspector(ev);
 });
+loose.onend = function () {
+  console.error("end");
+  console.error(loose);
+};
+
+// do this in random bits at a time to verify that it works.
+(function () {
+  if (xml) {
+    var c = Math.ceil(Math.random() * 1000)
+    loose.write(xml.substr(0,c));
+    xml = xml.substr(c);
+    process.nextTick(arguments.callee);
+  } else loose.close();
+})();
