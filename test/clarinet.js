74d806472096a00219f364c1ae8e70586fa2543c
David King <davidgwking@gmail.com>
2014-12-14 01:24:12 -0600

    atom overwrote character again
    -_-

--- test/clarinet.js
+++ test/clarinet.js
@@ -65,10 +65,10 @@ var seps   = [undefined, /\t|\n|\r/, '']
         ]
       }
     , four_byte_utf8 :
-      { text          : '{ "U+10ABCD": "ôŠ¯" }'
+      { text          : '{ "U+10ABCD": "í¯ªí¿" }'
       , events        :
         [ ["openobject"  , "U+10ABCD"]
-        , ["value"       , "ôŠ¯"]
+        , ["value"       , í¯ªí¿""]
         , ["closeobject" , undefined]
         , ['end'         , undefined]
         , ['ready'       , undefined]
