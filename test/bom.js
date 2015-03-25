54702308dfad7ec0c7fb6162d56635984aac3779
Anton Khlynovskiy <subzey@gmail.com>
2015-03-25 12:18:23 +0300

    Add test for BOM in the middle of the stream

--- test/bom.js
+++ test/bom.js
@@ -1,7 +1,19 @@
+// BOM at the very begining of the stream should be ignored
 require(__dirname).test({
   xml: '\uFEFF<Р></Р>',
   expect: [
     ['opentag', {'name':'Р', attributes:{}, isSelfClosing: false}],
+    ['closetag', 'Р']
+  ]
+});
+
+// In all other places it should be consumed
+require(__dirname).test({
+  xml: '\uFEFF<Р BOM="\uFEFF">\uFEFFStarts and ends with BOM\uFEFF</Р>',
+  expect: [
+    ['attribute', {'name':'BOM', 'value':'\uFEFF'}],
+    ['opentag', {'name':'Р', attributes:{'BOM':'\uFEFF'}, isSelfClosing: false}],
+    ['text', '\uFEFFStarts and ends with BOM\uFEFF'],
     ['closetag', 'Р']
   ]
 });
