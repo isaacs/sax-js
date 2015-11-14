877518790a38ed552dbd98a82c2cfc17bb2f80f5
Hasan Arous <arous.hasan@gmail.com>
2015-11-14 15:15:32 +0200

    Check for invalid code points before passing them to String.fromCodePoint
    - Now the call will not throw RangeError exceptions
    - Tests are included for both strict: true and strict: false modes
    - Fixes #116 and #160
    
    PR-URL: https://github.com/isaacs/sax-js/pull/163
    Credit: @aularon
    Close: #163
    Reviewed-by: @isaacs

---
+++ test/entities-invalid.js
@@ -0,0 +1,28 @@
+var invalidEntities = ['1114112', '-1', 'NaN']
+
+for (var i = invalidEntities.length - 1; i >= 0; --i) {
+  require(__dirname).test({
+    xml: '<r>&#' + invalidEntities[i] + ';</r>',
+    strict: false,
+    expect: [
+      ['opentag', { name: 'R', attributes: {}, isSelfClosing: false }],
+      ['text', '&#' + invalidEntities[i] + ';'],
+      ['closetag', 'R'],
+    ],
+  })
+  require(__dirname).test({
+    xml: '<r>&#' + invalidEntities[i] + ';</r>',
+    strict: true,
+    expect: [
+      ['opentag', { name: 'r', attributes: {}, isSelfClosing: false }],
+      [
+        'error',
+        'Invalid character entity\nLine: 0\nColumn: ' +
+          (6 + invalidEntities[i].length) +
+          '\nChar: ;',
+      ],
+      ['text', '&#' + invalidEntities[i] + ';'],
+      ['closetag', 'r'],
+    ],
+  })
+}
