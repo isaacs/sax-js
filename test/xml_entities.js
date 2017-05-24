301b0b06ede4d517f61521f981c7d917f98af04b
Niels Huylebroeck <niels.huylebroeck@nbn.be>
2017-05-24 09:12:34 +0200

    Added correct testing of &rsqb; entity

--- test/xml_entities.js
+++ test/xml_entities.js
@@ -6,7 +6,7 @@ require(__dirname).test({
   expect: [
     ['opentagstart', {'name': 'R', attributes: {}}],
     ['opentag', {'name': 'R', attributes: {}, isSelfClosing: false}],
-    ['text', '&rfloor; &spades; &copy; &rarr; & < < <  <   < > ] &real; &weierp; &euro;'],
+    ['text', '&rfloor; &spades; &copy; &rarr; & < < <  <   < > &rsqb; &real; &weierp; &euro;'],
     ['closetag', 'R']
   ]
 })
