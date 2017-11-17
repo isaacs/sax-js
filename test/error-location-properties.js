b313a3d927f7d32538f45970b54b6c8718b329b9
Marek <marekz@gmail.com>
2017-11-17 08:11:59 -0500

    Add test for error location properties

---
+++ test/error-location-properties.js
@@ -0,0 +1,17 @@
+var sax = require('../lib/sax')
+var t = require('tap')
+
+t.plan(4)
+
+var parser = sax.parser(true)
+
+parser.onerror = function (error) {
+  t.equal(error.message, 'Unexpected close tag\nLine: 2\nColumn: 7\nChar: >')
+  t.equal(error.line, 2)
+  t.equal(error.column, 7)
+  t.equal(error.char, '>')
+  parser.resume()
+}
+
+parser.write('<root>\n<p>Hello, world\n</root>').close();
+
