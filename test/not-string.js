57bab1b6d34f150efed67dfce56b6f7867062d07
Michael Nisi <michael.nisi@gmail.com>
2018-03-01 09:16:32 +0100

    Update Buffer usage

--- test/not-string.js
+++ test/not-string.js
@@ -1,9 +1,8 @@
 var parser = require('../').parser(true)
 var t = require('tap')
-var Buffer = require('safe-buffer').Buffer
 t.plan(1)
 parser.onopentag = function (node) {
   t.same(node, { name: 'x', attributes: {}, isSelfClosing: false })
 }
-var xml = new Buffer('<x>y</x>')
+var xml = Buffer.from('<x>y</x>')
 parser.write(xml).close()
