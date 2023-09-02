b4dd742a420b30655b4fed47e777dc4eab7afe83
Heiko Theißen <HeikoTheissen@web.de>
2023-09-02 13:34:24 +0200

    Entities with XML elements

---
+++ test/entity-elem.js
@@ -0,0 +1,18 @@
+var sax = require('../');
+sax.ENTITIES.attr = "1";
+sax.ENTITIES.text = "2";
+sax.ENTITIES.elem = '<B ATTR="&attr;.3"/>';
+require(__dirname).test({
+  xml: `<A ATTR="&attr;.2">&text;&elem;</A>`,
+  expect: [
+    ["opentagstart", {name: "A", attributes: {}}],
+    ["attribute", {name: 'ATTR', value: "1.2"}],
+    ["opentag", {name: "A", attributes: {ATTR: "1.2"}, isSelfClosing: false}],
+    ["text", "2"],
+    ["opentagstart", {name: "B", attributes: {}}],
+    ["attribute", {name: 'ATTR', value: "1.3"}],
+    ["opentag", {name: "B", attributes: {ATTR: "1.3"}, isSelfClosing: true}],
+    ["closetag", "B"],
+    ["closetag", "A"]
+  ]
+});
