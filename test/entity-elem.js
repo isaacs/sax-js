9404787a919702dac72e6dc63869b0973bb6c9a4
Heiko Theißen <HeikoTheissen@web.de>
2023-09-02 13:49:44 +0200

    Entities involving other entities

--- test/entity-elem.js
+++ test/entity-elem.js
@@ -1,6 +1,6 @@
 var sax = require('../');
 sax.ENTITIES.attr = "1";
-sax.ENTITIES.text = "2";
+sax.ENTITIES.text = "2.&attr;";
 sax.ENTITIES.elem = '<B ATTR="&attr;.3"/>';
 require(__dirname).test({
   xml: `<A ATTR="&attr;.2">&text;&elem;</A>`,
@@ -8,7 +8,7 @@ require(__dirname).test({
     ["opentagstart", {name: "A", attributes: {}}],
     ["attribute", {name: 'ATTR', value: "1.2"}],
     ["opentag", {name: "A", attributes: {ATTR: "1.2"}, isSelfClosing: false}],
-    ["text", "2"],
+    ["text", "2.1"],
     ["opentagstart", {name: "B", attributes: {}}],
     ["attribute", {name: 'ATTR', value: "1.3"}],
     ["opentag", {name: "B", attributes: {ATTR: "1.3"}, isSelfClosing: true}],
---
+++ test/entity-recursive.js
@@ -0,0 +1,12 @@
+var sax = require('../');
+sax.ENTITIES.attr = "1";
+sax.ENTITIES.text = "2.&attr;";
+require(__dirname).test({
+  xml: `<A>&text;</A>`,
+  expect: [
+    ["opentagstart", {name: "A", attributes: {}}],
+    ["opentag", {name: "A", attributes: {}, isSelfClosing: false}],
+    ["text", "2.1"],
+    ["closetag", "A"]
+  ]
+});
