8e28179fc3197fa4eeca2e0e1d5fac59120992c2
Michael Nestler <194211291+phactum-mnestler@users.noreply.github.com>
2025-10-29 09:37:11 +0100

    fix: avoid quadratic memory allocation in large CDATA block
    Instead of appending to the cdata buffer one character at a time,
    read entire 'safe' blocks (without ] characters) analogous to the
    text parsing logic.
    An added unit test ensures the memory allocation stays reasonable,
    but necessitates serial test execution.
    
    PR-URL: https://github.com/isaacs/sax-js/pull/279
    Credit: @phactum-mnestler
    Close: #279
    Reviewed-by: @isaacs

---
+++ test/cdata-mega.js
@@ -0,0 +1,23 @@
+var process = require('process')
+var t = require('tap')
+var sax = require('../lib/sax')
+
+t.test('cdata-mega', t => {
+  var bytesInMiB = 1024 * 1024
+  var cdataSize = 1 * bytesInMiB
+  var expectedUpperBound = cdataSize * 2;
+  var cdataContent = 'X'.repeat(cdataSize)
+  var xml = '<r><![CDATA[' + cdataContent + ']]></r>'
+
+  var memoryUsageBefore = process.memoryUsage().heapUsed
+
+  var parser = sax.parser()
+  var parsedCData = null
+  parser.oncdata = c => { parsedCData = c }
+  parser.write(xml).close()
+  var memoryUsageDiff = process.memoryUsage().heapUsed - memoryUsageBefore
+  
+  t.equal(parsedCData, cdataContent)
+  t.ok(memoryUsageDiff < expectedUpperBound, 'Expected at most ' + (expectedUpperBound / bytesInMiB) + ' MiB to be allocated, was ' + (memoryUsageDiff / bytesInMiB))
+  t.end()
+})
